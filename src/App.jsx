1990eb6eef10fb9c10aad1c20c11f3cf3b9dc51e
James Brooks <virverum22@gmail.com>
2025-04-07 13:26:06 -0400

    This is my first commit for this project :)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Layout from './components/Layout'
+
+function App() {
+// This is where the logic happens
+  return (
+    <Layout>
+      <h2>Welcome to the Blackjack Game!</h2>
+    </Layout>
+  )
+}
+
+export default App
